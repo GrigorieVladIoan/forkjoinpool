cf5e5db5a3bff6331bbb93b39c4cf6811127143d
Grigorie Vlad Ioan <vlad_grigorie@yahoo.com>
2024-04-01 12:23:52 +0300

    Initial commit

---
+++ src/main/java/com/learning/multithread/forkwoinpool/ForkwoinpoolApplication.java
@@ -0,0 +1,13 @@
+package com.learning.multithread.forkwoinpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForkwoinpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForkwoinpoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/multithread/forkwoinpool/ForkwoinpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.multithread.forkwoinpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForkwoinpoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
